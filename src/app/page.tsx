be2e08756154976d4a18711969c36a29e7d68a45
nathanlutz <nathanlutzg@gmail.com>
2023-12-14 18:27:26 -0800

    add sign in/up button, lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import { auth } from '@clerk/nextjs'
-import Image from 'next/image'
-import { redirect } from 'next/navigation';
+import { auth } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const {userId} = auth();
-  if (userId){
-    redirect('/dashboard');
+  const { userId } = auth();
+  if (userId) {
+    redirect("/dashboard");
   }
   return (
     <main className="">
-        <h1>clarity markets</h1>
+      <h1>clarity markets</h1>
     </main>
-  )
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,12 @@ export default authMiddleware({
   publicRoutes: ["/"],
   authorizedParties: [
     "http://localhost:3000",
+    "https://clarity-client-nathanlutzs-projects.vercel.app",
     "https://clarity-client-nathanlutzs-projects.vercel.app/",
     "https://clarity-client-git-master-nathanlutzs-projects.vercel.app",
+    "https://clarity-client-git-master-nathanlutzs-projects.vercel.app/",
     "https://clarity-client-2idwcleac-nathanlutzs-projects.vercel.app",
+    "https://clarity-client-2idwcleac-nathanlutzs-projects.vercel.app/",
   ],
 });
 
