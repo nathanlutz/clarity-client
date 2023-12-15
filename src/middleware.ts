466cc90a3f8599637921eca9eb323debf7bf7289
nathanlutz <nathanlutzg@gmail.com>
2023-12-14 17:51:58 -0800

    potential solution to prod deploy auth issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import { authMiddleware }    from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ['/'],
+  publicRoutes: ["/"],
+  authorizedParties: [
+    "http://localhost:3000",
+    "https://clarity-client-nathanlutzs-projects.vercel.app/",
+    "https://clarity-client-git-master-nathanlutzs-projects.vercel.app",
+    "https://clarity-client-2idwcleac-nathanlutzs-projects.vercel.app",
+  ],
 });
 
 export const config = {
