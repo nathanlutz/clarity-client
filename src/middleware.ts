85df216c187f5a3342c4f6025f89279689b37e6d
Nathan Lutz <nathanlutz@microsoft.com>
2024-01-03 17:28:19 -0800

    make feed accessible again

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware, redirectToSignIn }    from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/dashboard"],
+  publicRoutes: ["/", "/dashboard", "/feed"],
   authorizedParties: [
     "http://localhost:3000",
     "https://clarity-client-nathanlutzs-projects.vercel.app",
@@ -13,18 +13,7 @@ export default authMiddleware({
     "https://clarity-client-2idwcleac-nathanlutzs-projects.vercel.app/",
     "https://www.clarity-markets.com/",
     "https://www.clarity-markets.com"
-  ],
-  afterAuth: (user, req, res) => {
-    // If the user is not logged in and trying to access a protected route, don't allow them to access route
-    if (!user.userId && !user.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // If the user is logged in and trying to access a protected route, allow them to access route
-    if (user.userId && !user.isPublicRoute) {
-      return NextResponse.next()
-    }
-    return NextResponse.next();
-  }
+  ]
 });
 
 export const config = {
